f999cb5efaaafdc83ae8493c0ee9ca5ac1c7b7c0
dontwakemeup <103929054+dontwakemeup@users.noreply.github.com>
2024-02-03 18:30:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from openai import OpenAI
 st.title("ChatGPT-like clone clone")
 
 # Set OpenAI API key from Streamlit secrets
-client = OpenAI(api_key='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')
-
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 # Set a default model
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
